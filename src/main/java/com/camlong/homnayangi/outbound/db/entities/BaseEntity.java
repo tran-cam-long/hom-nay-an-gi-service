ef978624d4fed48b25e3f263b47a5177c53a55d5
Long Tran <camlong.tran0702@gmail.com>
2026-01-25 22:55:59 +0700

    [US3] feature: fix inheritance mapping lombok

--- src/main/java/com/camlong/homnayangi/outbound/db/entities/BaseEntity.java
+++ src/main/java/com/camlong/homnayangi/outbound/db/entities/BaseEntity.java
@@ -2,6 +2,7 @@ package com.camlong.homnayangi.outbound.db.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,7 @@ import java.time.Instant;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity implements Serializable {
 
